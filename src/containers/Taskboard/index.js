9c5c7d3ab4f1461bc62e0ef73c12207dc32a5b84
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2020-10-08 22:49:26 +0700

    UI - TaskBoard 1

--- src/containers/Taskboard/index.js
+++ src/containers/Taskboard/index.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
-import { withStyles } from '@material-ui/core';
+import { Button, Grid, withStyles } from '@material-ui/core';
 import styles from './styles';
+import { STATUSES } from '../../contants';
+import AddIcon from '@material-ui/icons/Add';
 
 class Taskboard extends Component {
+  renderBoard() {
+    return (
+      <Grid container spacing={2}>
+        {STATUSES.map((status, index) => (
+          <Grid item md={4} xs={12} key={index}>
+            {status.label}
+          </Grid>
+        ))}
+      </Grid>
+    );
+  }
+
   render() {
     const { classes } = this.props;
 
-    return <div className={classes.demo}>Taskboard</div>;
+    return (
+      <div className={classes.taskBoard}>
+        <Button variant='contained' color='primary' startIcon={<AddIcon />}>
+          Add task
+        </Button>
+        {this.renderBoard()}
+      </div>
+    );
   }
 }
 
