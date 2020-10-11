9054ff55f2586ff65c34b07fd3f497d232ce1a93
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2020-10-11 11:29:21 +0700

    UI - TaskForm

--- src/containers/Taskboard/index.js
+++ src/containers/Taskboard/index.js
@@ -4,6 +4,7 @@ import styles from './styles';
 import { STATUSES } from '../../contants';
 import AddIcon from '@material-ui/icons/Add';
 import TaskList from '../../components/TaskList';
+import TaskForm from '../../components/TaskForm';
 
 const taskList = [
   {
@@ -31,7 +32,24 @@ const taskList = [
     status: 2,
   },
 ];
+
 class Taskboard extends Component {
+  state = {
+    open: false,
+  };
+
+  handleClickOpen = () => {
+    this.setState({
+      open: true,
+    });
+  };
+
+  handleClose = () => {
+    this.setState({
+      open: false,
+    });
+  };
+
   renderBoard() {
     const { classes } = this.props;
 
@@ -49,15 +67,25 @@ class Taskboard extends Component {
     );
   }
 
+  renderForm() {
+    return <TaskForm open={this.state.open} onCloseForm={this.handleClose} />;
+  }
+
   render() {
     const { classes } = this.props;
 
     return (
       <div className={classes.taskBoard}>
-        <Button variant='contained' color='primary' startIcon={<AddIcon />}>
+        <Button
+          variant='contained'
+          color='primary'
+          startIcon={<AddIcon />}
+          onClick={this.handleClickOpen}
+        >
           Add task
         </Button>
         {this.renderBoard()}
+        {this.renderForm()}
       </div>
     );
   }
